28ca95a1d0db3d3386cb9804e93c16c48aacf025
Snezhana Pashovska <snezhana.pashovska@hotmail.com>
2022-02-03 11:45:41 +0100

    update js

--- js/blocs.js
+++ js/blocs.js
@@ -226,3 +226,18 @@ function initMap() {
       map.setCenter(marker.getPosition());
     });
   }
+
+
+  (function () {
+    if (typeof EventTarget !== "undefined") {
+        let func = EventTarget.prototype.addEventListener;
+        EventTarget.prototype.addEventListener = function (type, fn, capture) {
+            this.func = func;
+            if(typeof capture !== "boolean"){
+                capture = capture || {};
+                capture.passive = false;
+            }
+            this.func(type, fn, capture);
+        };
+    };
+}());
